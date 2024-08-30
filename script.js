7cba268bfff530b6091787c3a03366fd7551c89f
Medido1 <medi1222@gmail.com>
2024-08-30 12:12:50 +0100

    Set up header with nav menu for mobile and desktop

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 const toggleMenuBtn = document.querySelector(".toggle_menu_btn");
 const toggleBtnImg = document.querySelector(".toggle_btn");
+const mobileMenu = document.querySelector(".mobile_menu");
 const iconSrcs = 
 [{id: 1, src:"./images/icon-hamburger.svg"},
-  {id: 2, src:"./images/icon-close-menu.svg"}
+ {id: 2, src:"./images/icon-close-menu.svg"}
 ];
-let currentIcon = 1;
+let isMenuOpen = false;
 
 function changeIcon() {
-  if (currentIcon === 1){
-    toggleBtnImg.src = iconSrcs[1].src;
-    currentIcon += 1;
-  } else {
-    toggleBtnImg.src = iconSrcs[0].src;
-    currentIcon -= 1;
-  }
+  toggleBtnImg.src = isMenuOpen ? iconSrcs[0].src : iconSrcs[1].src;
+  isMenuOpen = !isMenuOpen;
 }
 
 toggleMenuBtn.addEventListener("click", () => {
   changeIcon();
+  mobileMenu.classList.toggle("hidden");
 })
