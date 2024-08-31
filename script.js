91952daef321ff6fdd9552cc3e694dae58c29e42
Medido1 <medi1222@gmail.com>
2024-08-31 07:58:19 +0100

    Add functionality to modal
      - Clicking on a pledge style it as the slected pledge
      - Clicking on a pledge shows input to type the sum
      - clicking continue updates the number of remaining pledges

--- script.js
+++ script.js
@@ -4,12 +4,27 @@ const mobileMenu = document.querySelector(".mobile_menu");
 const modal = document.querySelector(".modal");
 const backBtn = document.querySelector(".back"); /* back this project btn */
 const closeModal = document.querySelector(".close");
+const pledgeContainers = modal.querySelectorAll(".pledge_container");
+const radioBtns = modal.querySelectorAll("input[type='radio']");
+const inputGrps = modal.querySelectorAll(".input_grp");
+const continueBtns = modal.querySelectorAll(".continue");
+const remainingTexts = modal.querySelectorAll(".remaining");
 const iconSrcs = 
-[{id: 1, src:"./images/icon-hamburger.svg"},
- {id: 2, src:"./images/icon-close-menu.svg"}
+[
+  {src:"./images/icon-hamburger.svg"},
+  {src:"./images/icon-close-menu.svg"}
 ];
-
+const remainingPledges = 
+[
+  {name: "bamboo_stand", remaining: 101},
+  {name: "black_edition_stand", remaining: 64},
+  {name: "mahogany_special_edition", remaining: 0}
+]
 let isMenuOpen = false;
+
+remainingTexts.forEach((text, index) => {
+  text.textContent = remainingPledges[index].remaining;
+})
 
 function changeIcon() {
   toggleBtnImg.src = isMenuOpen ? iconSrcs[0].src : iconSrcs[1].src;
@@ -24,9 +39,36 @@ function hideModal() {
   modal.style.display = "none";
 }
 
+function selectPledge(index) {
+  if (index > 0 && remainingPledges[index - 1].remaining === 0){
+    return;
+  }
+  pledgeContainers.forEach(container => {
+    container.classList.remove("selected");
+  })
+  pledgeContainers[index].classList.add("selected");
+}
+
 toggleMenuBtn.addEventListener("click", () => {
   changeIcon();
   mobileMenu.classList.toggle("hidden");
 })
+
 backBtn.addEventListener("click", showModal);
 closeModal.addEventListener("click", hideModal);
+
+inputGrps.forEach((grp,index) => {
+  grp.addEventListener("click", () => {
+    const radioBtn = grp.querySelector("input[type='radio']");
+    radioBtn.checked = true;
+    selectPledge(index)
+  })
+})
+continueBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    if (index > 0) {
+      remainingPledges[index - 1].remaining -= 1;
+      remainingTexts[index -1].textContent = remainingPledges[index - 1].remaining;
+    }
+  })
+})
